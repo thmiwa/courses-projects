594cc492c879e536721549ee6d213603ffc3e5a4
thmiwa <57623590+thmiwa@users.noreply.github.com>
2020-02-13 22:05:34 -0300

    Add files via upload
    Netflix Categories Project from Introdução ao VueJS2

---
+++ introducao-vuejs2/netflix-categories/src/componentes/Categoria.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="slider">
+      
+      <h3>{{titulo}}</h3>
+      <!-- <teste titulo="Categoria"></teste> -->
+
+      <span v-on:mouseover="scrollEsquerda()" v-on:mouseout="clearScroll()" class="handle handlePrev active">
+        <i class="fa fa-caret-left" aria-hidden="true"></i>
+      </span>
+      
+      <div ref="scroller" class="row">
+        <div class="row__inner">
+
+          <filme v-for="filme in filmes" v-bind:key="filme.id" v-bind:titulo="filme.titulo" v-bind:imagem="filme.imagem"></filme>
+
+        </div>
+      </div>
+
+      <span v-on:mouseover="scrollDireita()" v-on:mouseout="clearScroll()" class="handle handleNext active">
+        <i class="fa fa-caret-right" aria-hidden="true"></i>
+      </span>
+
+    </div>    
+</template>
+
+<script>
+
+import Filme from './Filme.vue'
+
+export default {
+    // name: 'teste',
+    props:['titulo','filmes'],
+    components: {
+        Filme
+    },
+    methods: {
+    scrollDireita() {
+        // const self = this;  
+        // this.intervalo = setInterval(function(){ self.$refs.scroller.scrollLeft += 1 }  , 5);
+        this.intervalo = setInterval(() => { this.$refs.scroller.scrollLeft += 1 }  , 5);
+    },
+    scrollEsquerda() {
+        // const self = this;
+        // this.intervalo = setInterval(function(){ self.$refs.scroller.scrollLeft -= 1 }  , 5);
+        this.intervalo = setInterval(() => { this.$refs.scroller.scrollLeft -= 1 }  , 5);
+    },
+    clearScroll() {
+      clearInterval(this.intervalo);
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
+
---
+++ introducao-vuejs2/netflix-categories/src/componentes/Filme.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="card" v-bind:style="efeitoClick" v-on:click="clickEfeito()" v-on:mouseout="removeClickEfeito()">
+        <div class="card__media">
+            <img class="card__img" v-bind:src="imagem" alt=""  />
+        </div>
+        <div class="card__details">
+            <div class="card__title">
+                {{titulo}}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['titulo','imagem'],
+    data() {
+        return {
+            efeitoClick: null,
+        }
+    },
+    methods: {
+        clickEfeito: function() {
+            this.efeitoClick = 'transform: scale(1.9); margin-right: 110px;';            
+        },
+        removeClickEfeito: function() {
+            this.efeitoClick = null;
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
